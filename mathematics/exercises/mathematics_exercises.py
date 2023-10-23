0dab5c9c7d0f51917259ea4feb56e7b91d3dc501
Anna Karutina <anna.karutina@khk.ee>
2023-10-23 13:20:23 +0300

    math exercises - solution

---
+++ mathematics/exercises/mathematics_exercises.py
@@ -0,0 +1,93 @@
+import math
+"""Math exercises."""
+
+
+def sum_and_difference(num_a: int, num_b: int) -> tuple:
+    """Return the sum and difference of given variables num_a and num_b."""
+    sum = num_a + num_b
+    difference = num_a - num_b
+    return sum, difference
+
+
+def float_division(num_a: int, num_b: int) -> float:
+    """Divide given variables num_a and num_b and return the result."""
+    division =  num_a / num_b
+    return division
+
+
+def integer_division(num_a: int, num_b: int) -> int:
+    """Divide given variables num_a and num_b and return the result rounded down."""
+    division =  num_a // num_b
+    return division
+
+
+def powerful_operations(num_a: int, num_b: int) -> tuple:
+    """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
+    multiply_numbers = num_a * num_b
+    power = num_a ** num_b
+    remainder = num_a % num_b
+    return multiply_numbers, power, remainder
+
+
+def find_average(num_a: int, num_b: int) -> float:
+    """Return the average of given variables."""
+    average = (num_a + num_b) / 2
+    return average
+
+
+def area_of_a_circle(radius: float) -> float:
+    """Calculate and return the area of a circle."""
+    circle_area = round(math.pi * math.pow(radius, 2), 2)
+    return circle_area
+
+
+def area_of_an_equilateral_triangle(side_length: float) -> int:
+    """Calculate and return the area of an equilateral triangle."""
+    s = 3 * side_length / 2
+    triangle_area = round(math.sqrt(s * (s - side_length) ** 3))
+    return triangle_area
+
+
+def calculate_discriminant(a: int, b: int, c: int) -> int:
+    """Calculate discriminant with given variables and return the result."""
+    discriminant = b ** 2 - 4 * a * c
+    return discriminant
+
+
+def calculate_hypotenuse_length(a: int, b: int) -> float:
+    """Return the length of hypotenuse when the lengths of the catheti are given."""
+    c = math.sqrt(a ** 2 + b ** 2)
+    return c
+
+
+def calculate_cathetus_length(a: int, c: int) -> float:
+    """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
+    b = math.sqrt(c ** 2 - a ** 2)
+    return b
+
+
+def time_converter(seconds: int) -> str:
+    """Convert time in seconds to hours and minutes."""
+    minutes = seconds // 60
+    hours = minutes // 60
+    minutes = minutes % 60
+    return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
+
+
+def student_helper(angle: int) -> str:
+    """Return the sine and cosine of the given angle in degrees."""
+    sine = round(math.sin(math.radians(angle)), 1)
+    cosine = round(math.cos(math.radians(angle)), 1)
+    return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
+
+
+def greetings(n: int) -> str:
+    """Return a string that contains "Hey" n times."""
+    lots_of_heys = "Hey" * n
+    return lots_of_heys
+
+
+def adding_numbers(num_a: int, num_b: int) -> str:
+    """Return given numbers added together as a string."""
+    string_numbers = str(num_a) + str(num_b)
+    return string_numbers
